8f381c8f47ebf3872e291f1804e23569994577f1
Juan Andrés Losauro <losaurojuan@gmail.com>
2025-03-17 14:02:08 -0300

    commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-amber-500 justify-center flex-1 items-center">
+      <Text className="font-bold text-white text-3xl">Pollo</Text>
+    </View>
+  );
+}
